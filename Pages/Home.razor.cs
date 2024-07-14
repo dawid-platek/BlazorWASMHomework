33d7c9cf4285e1d17d2e03aece909e9d79a9646a
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-07-14 19:00:51 +0200

    Add project files.

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASMHomework.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        private void GoToHomework()
+        {
+            NavigationManager.NavigateTo("/homework/parameter");
+        }
+    }
+}
---
+++ Pages/Homework.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASMHomework.Pages
+{
+    public partial class Homework
+    {
+        [Parameter]
+        public string Header { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        private void GoToHome()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+
+    }
+
+}
